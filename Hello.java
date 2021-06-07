d01e921e249bc035822ed648018037318635f6a1
dende-h <84645413+dende-h@users.noreply.github.com>
2021-06-07 09:45:37 +0900

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,37 @@
+package jp.issue.raisetech;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloWorld!");
+		System.out.print("\n");
+		int n = 0;
+		while(n < 5) {
+			int i = 0;
+		
+			for(; i < 4; i++) {
+				System.out.println("HelloWorld!");
+			}
+		System.out.print("\n");
+			for(; i < 10; i++) {
+				if(i % 2 == 0) {
+					System.out.println("HelloWorld?");
+				}else {
+					System.out.println("こんにちは世界!");
+				}
+				
+				if(i == 7) {
+					break;
+				}
+		
+			}
+		System.out.println("********************************");	
+			n++;
+		}
+		System.out.println("END");
+		
+		
+	}
+
+}
